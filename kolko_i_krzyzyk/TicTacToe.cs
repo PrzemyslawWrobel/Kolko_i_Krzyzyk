0090afb1386287619fc6f08ac01cac1ce33278fd
PW <przemek.wrobel@poczta.fm>
2020-04-07 10:46:45 +0200

    CZ1

---
+++ kolko_i_krzyzyk/TicTacToe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolko_i_krzyzyk
+{
+    public class TicTacToe
+    {
+        private Square[][] board =
+        {
+            new Square[3],
+            new Square[3],
+            new Square[3]
+
+        };
+
+    }
+}
